cccd926ac6ff9efa74de4aa8b4f39d31fdbf4352
Amelia <paniagua10@hotmail.com>
2018-04-05 14:50:09 +0200

    05/04/2018 14:50

---
+++ src/main/java/trazas/Principal.java
@@ -0,0 +1,21 @@
+package trazas;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Principal {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Principal.class);
+
+	public static void main(String[] args) {
+		
+		logger.info("Entrando");
+		
+		System.out.println("Esto es una prueba");
+		
+		logger.info("Saliendo");
+		
+
+	}
+
+}
